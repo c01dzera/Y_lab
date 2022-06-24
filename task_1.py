829f0891865cb2e46a3600dfaf96dd397b068016
Vilen_Bakov <bakov.vilen@yandex.ru>
2022-06-24 15:58:18 +0300

    test: import re, first try

--- task_1.py
+++ task_1.py
@@ -2,10 +2,16 @@ import re
 
 
 def domain_name(url):
-    return
+    patter = r'\W(\w+?)\W'
+    return re.search(patter, url)[1]
 
 
-# assert domain_name("http://google.com") == "google"
-# assert domain_name("http://google.co.jp") == "google"
-# assert domain_name("www.xakep.ru") == "xakep"
-# assert domain_name("https://youtube.com") == "youtube"
+assert domain_name("http://google.com") == "google"
+assert domain_name("http://google.co.jp") == "google"
+assert domain_name("www.xakep.ru") == "xakep"
+assert domain_name("https://youtube.com") == "youtube"
+
+
+
+
+
