78675c9853304ab306a3c05d8a7653c8ae3fb9b1
Vilen_Bakov <bakov.vilen@yandex.ru>
2022-06-24 15:30:18 +0300

    test: create task_1

---
+++ task_1.py
@@ -0,0 +1,11 @@
+import re
+
+
+def domain_name(url):
+    return
+
+
+# assert domain_name("http://google.com") == "google"
+# assert domain_name("http://google.co.jp") == "google"
+# assert domain_name("www.xakep.ru") == "xakep"
+# assert domain_name("https://youtube.com") == "youtube"
