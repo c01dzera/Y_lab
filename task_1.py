788310db14afdf4cbbc2cb8bba1da6453faa2ffb
Vilen_Bakov <bakov.vilen@yandex.ru>
2022-06-26 13:48:28 +0300

    test: task_1 revision_1

--- task_1.py
+++ task_1.py
@@ -1,17 +1,16 @@
-import re
 
 
 def domain_name(url):
-    patter = r'\W(\w+?)\W'
-    return re.search(patter, url)[1]
+    return url.split("www.")[-1].split("//")[-1].split(".")[0]
 
 
 assert domain_name("http://google.com") == "google"
 assert domain_name("http://google.co.jp") == "google"
 assert domain_name("www.xakep.ru") == "xakep"
 assert domain_name("https://youtube.com") == "youtube"
-
-
-
-
-
+assert domain_name("") == ""
+assert domain_name("https://foobar.co.uk") == "foobar"
+assert domain_name("https://facebook.com?") == "facebook"
+assert domain_name("https://com.com.com") == "com"
+assert domain_name("https://shopping.online") == "shopping"
+assert domain_name("www.something.net/index.com") == "something"
