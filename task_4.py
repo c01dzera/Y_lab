0763fb320f7f5ce489a57ca9bb50cd6e7f6bdeed
Vilen_Bakov <bakov.vilen@yandex.ru>
2022-06-28 01:38:15 +0300

    test: task_4 ver.1

--- task_4.py
+++ task_4.py
@@ -1,13 +1,24 @@
 import itertools
 
 
-def bananas(s):
+def bananas(s) -> set:
     result = set()
-    length = len('banana')
-    if len(s) >= len('banana'):
-        for i in itertools.combinations(s, 6):
-            if 'banana' in ''.join(i):
-                print(''.join(i))
+
+    for inx in itertools.combinations(range(len(s)), len(s)-6):
+        ban = list(s)
+        for i in inx:
+            ban[i] = '-'
+        ban = ''.join(ban)
+        if ban.replace('-', '') == 'banana':
+            result.add(ban)
+
+    return result
 
 
-bananas('bbananana')
+assert bananas("banann") == set()
+assert bananas("banana") == {"banana"}
+assert bananas("bbananana") == {"b-an--ana", "-banana--", "-b--anana", "b-a--nana", "-banan--a",
+                                "b-ana--na", "b---anana", "-bana--na", "-ba--nana", "b-anan--a",
+                                "-ban--ana", "b-anana--"}
+assert bananas("bananaaa") == {"banan-a-", "banana--", "banan--a"}
+assert bananas("bananana") == {"ban--ana", "ba--nana", "bana--na", "b--anana", "banana--", "banan--a"}
