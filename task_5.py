63a00dd75f070bdcbc62ab74c82a364ce98ecd32
Vilen_Bakov <bakov.vilen@yandex.ru>
2022-06-28 02:48:10 +0300

    test: task_4 try_1

--- task_5.py
+++ task_5.py
@@ -1,3 +1,30 @@
+import itertools
+from functools import reduce
+
+
+def multiply(seq):
+    res = 1
+    for _ in seq:
+        res *= _
+    return res
+
+
 def count_find_num(primesL, limit):
     # your code here
-    return []
+    rep = 3
+    for nums in itertools.combinations_with_replacement(primesL, rep):
+        if multiply(primesL) <= multiply(nums) <= limit:
+            print(nums, multiply(nums))
+    print()
+    rep = 4
+    for nums in itertools.combinations_with_replacement(primesL, rep):
+        if multiply(primesL) <= multiply(nums) <= limit:
+            print(nums, multiply(nums))
+    print()
+    rep = 5
+    for nums in itertools.combinations_with_replacement(primesL, rep):
+        if (multiply(primesL) <= multiply(nums) <= limit) and (set(primesL).issubset(set(nums))):
+            print(nums, multiply(nums))
+
+
+count_find_num(primesL = [2, 5, 7], limit = 500)
